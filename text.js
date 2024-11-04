d97e7620832735343404e309f302fbd47c5af3b3
GannaKov <zlatta2000@gmail.com>
2024-11-04 14:27:38 +0100

    first commit

---
+++ text.js
@@ -0,0 +1,26 @@
+/* "module": "./build/three.module.js"; */
+
+/* "exports": {
+    ".": {
+      "import": "./build/three.module.js",
+      "require": "./build/three.cjs"
+    },
+    "./examples/fonts/*": "./examples/fonts/*",
+    "./examples/jsm/*": "./examples/jsm/*",
+    "./addons": "./examples/jsm/Addons.js",
+    "./addons/*": "./examples/jsm/*",
+    "./src/*": "./src/*",
+    "./webgpu": "./build/three.webgpu.js",
+    "./tsl": "./build/three.webgpu.js"
+  }, */
+
+/* "files":["build","examples/jsm",
+    "examples/fonts",
+    "LICENSE",
+    "package.json",
+    "README.md",
+    "src"
+  ], ; */
+
+/* "sideEffects":["./src/nodes/**/
+// *"], ; */
